ed05c800ee7ac2eba6fd9b6aa42ac4ca29c8f2ad
petit-polisson <tbranchereau2@gmail.com>
2024-11-15 17:43:05 +0100

    adding plot and bug fixing code ehancing

--- main.py
+++ main.py
@@ -3,6 +3,71 @@ import csv
 from picture import Picture
 from player import Player
 import random
+import matplotlib.pyplot as plt
+
+
+def plot_comparison(player_list, best_game,number_of_game):
+
+    if len(player_list) != len(best_game)or len(player_list)!=len(number_of_game):
+        print("Error : data lists not the same length.")
+        return
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(player_list, best_game, color="skyblue", edgecolor="black")
+
+
+    plt.xlabel("Players", fontsize=14)
+    plt.ylabel("Personal Best (Score)", fontsize=14)
+    plt.title("Player Personal Best Scores", fontsize=16)
+    
+    
+    for i, score in enumerate(best_game):
+        plt.text(i, score + 0.1, f"{score:.2f}\nnb of game{number_of_game[i]}", ha='center', fontsize=10, color='darkblue')
+    
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_evolution(jumps_evolution, position_evolution,player_name,average_jumps,average_position):
+
+    if len(jumps_evolution) != len(position_evolution):
+        print("Error : data lists not same length.")
+        return
+    
+
+    games = list(range(1, len(jumps_evolution) + 1))
+
+    plt.figure(figsize=(10, 6))
+    
+
+    plt.plot(games, jumps_evolution, marker='o', label="Jumps Evolution", color="blue")
+    plt.plot(games, position_evolution, marker='s', label="Position Evolution", color="green")
+
+    plt.xlabel("Game Number", fontsize=14)
+    plt.ylabel("Jumps and Position(*100)", fontsize=14)
+    plt.title(f'Evolution of Jumps and Position by Game for {player_name} ', fontsize=16)
+    plt.xticks(games, fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.legend(fontsize=12)
+    plt.grid(True, linestyle='--', alpha=0.6)
+
+    info_text = ""
+    info_text += f"Average Jumps: {average_jumps}\n"
+    info_text += f"Average Position: {average_position}"
+    
+    if info_text:
+        plt.text(
+            0.95, 0.95, info_text,
+            transform=plt.gca().transAxes,
+            fontsize=12,
+            color="darkred",
+            ha="right",
+            va="top",
+            bbox=dict(facecolor="white", edgecolor="darkred", boxstyle="round,pad=0.5")
+        )
+    
+    plt.tight_layout()
+    plt.show()
 
 def play(filename):
     pygame.mixer.init(frequency=16000)
@@ -38,6 +103,91 @@ def leaderboard_data(filename):
                 break 
     return data
 
+def set_timer():
+    stop_music()
+    pygame.time.set_timer(OPEN_EVENT, 1000)
+    pygame.time.wait(250)
+    play("sound/son.mp3")
+    pygame.time.wait(250)
+    pygame.time.set_timer(CLOSE_EVENT, 1000)
+
+
+def player_data(player_name):
+    total_jumps = 0
+    total_position = 0
+    number_of_game = 0
+    jumps_evolution = []
+    position_evolution = []
+    
+    try:
+        with open("csv_files/data.csv", "r", newline="") as csvfile:
+            reader = csv.reader(csvfile)
+            
+            for row in reader:
+                if len(row) < 3:
+                    continue
+                
+                if row[0] == player_name:
+                    jump_counter = int(row[1])
+                    position = int(row[2])  
+                    
+                    total_jumps += jump_counter
+                    total_position += position
+                    jumps_evolution.append(jump_counter)
+                    position_evolution.append(position/100)
+                    number_of_game += 1
+                    
+        if number_of_game > 0:
+            average_jumps = total_jumps / number_of_game
+            average_position = total_position / number_of_game
+            return int(average_jumps), int(average_position), number_of_game, jumps_evolution, position_evolution
+        else:
+          
+            return None, None, 0, [], []
+    except FileNotFoundError:
+        print("Erreur : le fichier data.csv est introuvable.")
+        return None, None, 0, [], []
+    except ValueError:
+        print("Erreur : données incorrectes dans le fichier CSV.")
+        return None, None, 0, [], []
+    
+def player_comparison():
+
+    player_list = []
+    best_game = []
+    number_of_game=[]
+    
+    try:
+        with open("csv_files/data.csv", "r", newline="") as csvfile:
+            reader = csv.reader(csvfile)
+            
+            for row in reader:
+                if len(row) < 3:  
+                    continue
+                
+                player_name = row[0]
+                position = int(row[2]) 
+                
+                
+              
+                if player_name in player_list:
+                    index = player_list.index(player_name)
+                    best_game[index] = max(best_game[index], position)
+                    number_of_game[index]+=1
+                else:
+                    player_list.append(player_name)
+                    best_game.append(position)
+                    number_of_game.append(1)
+    
+        return player_list, best_game, number_of_game
+    
+    except FileNotFoundError:
+        print("Erreur : le fichier data.csv est introuvable.")
+        return [], []
+    except ValueError:
+        print("Erreur : données incorrectes dans le fichier CSV.")
+        return [], []
+
 # Initialisation of pygame
 pygame.init()
 
@@ -50,15 +200,10 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Space Boy")
 OPEN_EVENT = pygame.USEREVENT + 1
-CLOSE_EVENT = pygame.USEREVENT + 2
-pygame.time.set_timer(OPEN_EVENT, 1000)
-pygame.time.wait(150)
-play("sound/son.mp3")
-pygame.time.wait(250)
-pygame.time.set_timer(CLOSE_EVENT, 1000)
+CLOSE_EVENT = pygame.USEREVENT +2
 open_event_active = False
 paused=False
-menu=False
+menu=True
 running = True
 dt = 0
 jump_counter = 0
@@ -98,24 +243,27 @@ while running:
                     stop_music()
                     play("sound/menu.mp3")
                 else:
-                    stop_music()
-                    play("sound/son.mp3")
+                    set_timer()
                 menu=not menu   
             elif event.key == pygame.K_p:
                 paused=not paused
-        
+
 
     if menu:
+
         screen.fill((0,0,0))
         font = pygame.font.Font(None, 200)
-        menu_text = font.render(f"Leaderboard", True, (255, 255, 255))
+        menu_text = font.render(f"Menu", True, (255, 255, 255))
         screen.blit(menu_text, (430, 100))
+        font = pygame.font.Font(None, 80)
+        enter_text = font.render(f"press m to enter", True, (255, 255, 255))
+        screen.blit(enter_text, (130, 250))
         leaderboard=leaderboard_data("csv_files/sorted_data.csv")
         for i, item in enumerate(leaderboard):
             print(i, item)
             font = pygame.font.Font(None, 50)
             u=font.render(f'Name: {item[0]}  Jump Counter: {item[1]} Position: {item[2]}', True, (255, 255, 255))
-            screen.blit(u, (100, 250+50*i))
+            screen.blit(u, (100, 350+50*i))
 
         
 
@@ -187,7 +335,6 @@ while running:
     # Mise à jour de l'écran
     pygame.display.flip()
 
-
 player_name = ""
 entering_name = True
 r=random.randint(0, 255)
@@ -222,6 +369,10 @@ while entering_name:
             if event.key == pygame.K_RETURN:
                 save_data(jump_counter, player_name,alien.rel_position(moon.position())-alien.relative_position)
                 sort_data()
+                average_jumps, average_position, number_of_game,jumps_evolution,position_evolution=player_data(player_name)
+                plot_evolution(jumps_evolution,position_evolution,player_name,average_jumps,average_position)
+                player_list,best_game,number_of_game =player_comparison()
+                plot_comparison(player_list,best_game,number_of_game)
                 entering_name = False
             elif event.key == pygame.K_BACKSPACE:
                 player_name = player_name[:-1]
